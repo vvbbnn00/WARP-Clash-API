c0b38b187af50c885e6e1aea3b50f1fb146efd85
vvbbnn00 <vvbbnn00@foxmail.com>
2024-02-27 17:37:52 +0800

    chore: Add GPL license statement at the beginning of all source code files.

--- utils/wireguard.py
+++ utils/wireguard.py
@@ -1,3 +1,19 @@
+"""
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <https://www.gnu.org/licenses>.
+
+"""
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import x25519
 from base64 import b64encode
