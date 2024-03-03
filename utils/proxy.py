20a800b27e12ea9d32a2c1d36319fdf9f15748b5
vvbbnn00 <vvbbnn00@foxmail.com>
2024-03-03 16:20:39 +0800

    feat: add timeout for getProxy function.

--- utils/proxy.py
+++ utils/proxy.py
@@ -24,11 +24,14 @@ def getProxy():
     Get proxy from proxy pool
     :return: proxy got from proxy pool
     """
-    ret = requests.get(PROXY_POOL_URL).json()
-    proxy = {}
-    if ret.get('proxy'):
-        if ret['https']:
-            proxy = {"https": {ret['proxy']}}
-        else:
-            proxy = {"http": {ret['proxy']}}
-    return proxy
+    try:
+        ret = requests.get(PROXY_POOL_URL, timeout=60).json()
+        proxy = {}
+        if ret.get('proxy'):
+            if ret['https']:
+                proxy = {"https": {ret['proxy']}}
+            else:
+                proxy = {"http": {ret['proxy']}}
+        return proxy
+    except TimeoutError:
+        return {}
